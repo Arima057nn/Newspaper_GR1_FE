9d11448aa8f60e58cfeb4e0486e387b25d37c663
patricknguyenx <90426346+patricknguyenx@users.noreply.github.com>
2023-07-28 16:11:51 +0700

    Update Detail.jsx

--- src/components/ArticleDetail/Detail.jsx
+++ src/components/ArticleDetail/Detail.jsx
@@ -5,7 +5,64 @@ const cx = classNames.bind(styles);
 function ArticleDetail() {
   return (
     <div className={cx("wrapper")}>
-      <div className={cx("left")}>h1h1</div>
+      <div className={cx("left")}>
+        <div className={cx("header-item")}>
+          <div className={cx("header-item1")}>Trang chủ</div>
+          <div className={cx("header-item2")}>Bóng đá</div>
+          <div className={cx("header-item3")}>Thứ Ba, ngày 18/07/2023 08:05 AM (GMT+7)</div>
+        </div>
+    
+      <div className={cx("tittle")}>
+      Ronaldo tuyên bố giải Ả Rập tốt hơn MLS của Messi, đá chính ngày Al Nassr thua 0-5
+      </div>
+
+      <div className={cx("content1")}>
+      Ronaldo có những chia sẻ đáng chú ý sau khi cùng Al Nassr thi đấu giao hữu với Celta Vigo.
+      </div>
+
+      <div className={cx("content2")}>
+      Al Nassr đã bước vào trận đấu giao hữu đầu tiên chuẩn bị cho mùa giải mới gặp Celta Vigo. Ở trận này, Ronaldo được xếp đá chính và thi đấu trong 45 phút. Bên cạnh Ronaldo, còn có cái tên khác đáng chú ý cũng xuất hiện. Đó là Marcelo Brozovic, tân binh của Al Nassr. 
+      </div>
+
+      <div className={cx("news")}>
+        <img src="https://icdn.24h.com.vn/upload/3-2023/images/2023-07-18/vdvsxsa-1689641282-525-width740height555.jpg" />
+      </div>
+
+      <div className={cx("content3")}>
+      Ronaldo vô duyên trong ngày Al Nassr thua đậm
+      </div>
+
+      <div className={cx("content2")}>
+      Ronaldo ở trận này có được 2 cơ hội đánh đầu gần khung thành đối phương khá ngon ăn ở phút 12 và 25. Tuy nhiên siêu sao người Bồ Đào Nha lại tỏ ra quá vô duyên. Cũng trong hiệp một, Al Nassr hòa Celta Vigo với tỷ số 0-0. 
+      </div>
+
+      <div className={cx("content2")}>
+      Sang hiệp 2, Ronaldo rời sân. Cũng ngay từ đầu hiệp hai, Al Nassr đã chịu thiệt quân khi Al Amri nhận thẻ đỏ. Điều đó khiến đội bóng Ả Rập đánh mất hoàn toàn thế trận vào tay Celta Vigo. Đội bóng Tây Ban Nha thoải mái thi đấu hơn và đã nã tới 5 bàn vào lưới Al Nassr, qua đó giành chiến thắng chung cuộc với tỷ số 5-0. 
+      </div>
+
+      <div className={cx("content2")}>
+      Sau trận gặp Celta Vigo, Al Nassr còn những trận giao hữu đáng chú ý khác. Họ sẽ đối đầu Benfica, PSG và Inter Milan. Các đối thủ này cũng không hề dễ đối phó, thậm chí còn khó chơi hơn so với Celta Vigo.
+      </div>
+
+      <div className={cx("content2")}>
+      Sau trận đấu này, Ronaldo đã có những chia sẻ đáng chú ý: "Tôi sẽ không bao giờ trở lại châu Âu. Cánh cửa ấy đã đóng lại hoàn toàn. Tôi hiện đã 38 tuổi, trong khi bóng đá châu Âu cũng đã giảm sút về chất lượng. Chỉ giải Ngoại hạng Anh là còn nhiều giá trị. Sân chơi này vượt xa những giải đấu khác. 
+      </div>
+
+      <div className={cx("content2")}>
+      Tôi đánh giá Saudi Pro League tốt hơn MLS. Tôi cũng xin khẳng định lại lần nữa tôi chắc chắn không khoác áo bất cứ đội bóng châu Âu nào nữa. Tôi là người mở đường tới giải Ả Rập, để rồi nhiều ngôi sao sáng giá nối gót tôi tới đây".
+      </div>
+
+      <div className={cx("content2")}>
+      Giải bóng đá nhà nghề Mỹ MLS chính là sân chơi mới của Messi khi anh cập bến Inter Miami. Hiện "El Pulga" mới có lễ ra mắt đội bóng mới chứ chưa thi đấu trận nào.
+      </div>
+
+      <div className={cx("content4")}>
+      Theo Phong Đức(Tạp chí Du lịch TP.HCM) 
+      </div>
+
+
+
+      </div>
       <div className={cx("right")}>h1h1</div>
     </div>
   );
